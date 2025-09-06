b6df8a8dea74d0dd00a65f72242be0a17f183d9d
JERINSAN <jerinsan1004@gmail.com>
2025-09-06 12:28:29 +0530

    Creater Video Added

--- src/Components/Creators.js
+++ src/Components/Creators.js
@@ -1,5 +1,5 @@
 import '../Assets/css/HowitsworkStyle_Creators.css';
-import CreatorsVideo from'../Assets/videos/tokn.mp4';
+import CreatorsVideo from'../Assets/videos/Creators.mp4';
 import "aos/dist/aos.css";
 import AOS from "aos";
 import { useEffect } from 'react';
